6c6eaf5ec62489a9e90e186f7e939635ebfae6d8
shashlyck <garlodey2@mail.ru>
2020-02-22 15:57:56 +0400

    first commit

---
+++ src/com/shashlyck/functions/SqrRootFunctionTest.java
@@ -0,0 +1,13 @@
+package com.shashlyck.functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SqrRootFunctionTest {
+
+    @Test
+    void apply() {
+
+    }
+}
